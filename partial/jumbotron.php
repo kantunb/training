b0118702c530e867370ee6a3b6260b38942c6340
Quentin Brouillet <quent07@hotmail.com>
2020-04-30 11:32:45 +0200

    create index and partials

---
+++ partial/jumbotron.php
@@ -0,0 +1,9 @@
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1>Aidez-nous !</h1>
+        <p class="lead text-muted">Pour ajouter un pentionnaire, cliquez ci-dessous</p>
+        <p>
+            <a href="./add.php" class="btn btn-primary my-2">Ajouter une bêbête</a>
+        </p>
+    </div>
+</section>
