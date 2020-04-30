edbf645df2a56c66b95e647d8f44dc9cdc6cafa0
Quentin Brouillet <quent07@hotmail.com>
2020-04-30 15:58:41 +0200

    edit form

--- add.php
+++ add.php
@@ -25,19 +25,19 @@
             <div class="container">
                 <div class="row">
 
-                    <form class="form-group" metod="" action="">
+                    <form class="form-group" action="./post/create.php" method="POST">
                         <fieldset class="p-3">
                             <legend>Ajouter un whisky</legend>
                             <p class="mt-3">
                                 <label for="name">Nom</label>
-                                <input type="text" class="form-control" name="name" id="" aria-describedby="name" placeholder="" required>
+                                <input type="text" class="form-control" name="name" id="" aria-describedby="name" value="test_nom" placeholder="" required>
                                 <!-- <small id="name" class="form-text text-muted"></small> -->
                             </p>
                             <p class="mt-3">
                                 <label for="category">Categorie</label>
-                                <select name="category" id="" class="ml-3 aria-describedby="category" required">
+                                <select name="category" id="" class="ml-3 aria-describedby="category" required>
                                     <option value="">select</option>
-                                    <option value="blend">Blended Malt</option>
+                                    <option value="blend" selected>Blended Malt</option>
                                     <option value="single_malt">Single Malt</option>
                                     <option value="bourbon">Bourbon</option>
                                     <option value="rye">Rye</option>
@@ -46,36 +46,36 @@
                             </p>
                             <p class="mt-3">
                                 <label for=" distillery">Distillerie</label>
-                                <input type="text" class="form-control" name="distillery" id="" aria-describedby="Distillerie" required>
+                                <input type="text" class="form-control" name="distillery" id="" aria-describedby="Distillerie" value="test_distillerie" required>
                             </p>
                             <p class="mt-3">
                                 <label for="bottled">Date de mise en bouteille</label>
-                                <input type="number" class="form-control" name="bottled" id="" min="4" max="4" aria-describedby="Date de mise en bouteille">
+                                <input type="number" class="form-control" name="bottled" id="" min="1800" max="2100" aria-describedby="Date de mise en bouteille">
                                 <small id="date_bouteille" class="form-text text-muted">Ex : 2010</small>
                             </p>
                             <p class="mt-3">
                                 <label for="stated_age">Âge</label>
-                                <input type="number" class="form-control" name="stated_age" id="" min="4" max="4" aria-describedby="Âge">
+                                <input type="number" class="form-control" name="stated_age" id="" min="0" max="200" aria-describedby="Âge">
                                 <small id="age_bouteille" class="form-text text-muted">Ex : 15</small>
                             </p>
                             <p class="mt-3">
                                 <label for="strength">Degré d'alcool</label>
-                                <input type="number" class="form-control" name="strength" id="" min="2" max="4" aria-describedby="Degré d'alcool" required>
+                                <input type="number" class="form-control" name="strength" id="" min="30" max="80" step="0.01" aria-describedby="Degré d'alcool" value="20" required>
                                 <small id="degree_bouteille" class="form-text text-muted">Ex : 42,5</small>
                             </p>
                             <p class="mt-3">
                                 <label for="flavor">Arômes</label>
-                                <input type="text" class="form-control" name="flavor" id="" aria-describedby="Arômes" required>
+                                <input type="text" class="form-control" name="flavor" id="" aria-describedby="Arômes" value="test_aromes" required>
                             </p>
                             <p class="mt-3">
                                 <label for="description">Description</label>
-                                <input type="text" class="form-control" name="description" id="" aria-describedby="Description">
+                                <textarea class="form-control" name="description" id="" aria-describedby="Description"></textarea>
                             </p>
                             <p class="mt-3">
                                 <label for="rate">Note</label>
                                 <select name="rate" id="" class="ml-3 aria-describedby="Note" required>
                                     <option value="">select</option>
-                                    <option value="0">0</option>
+                                    <option value="0" selected>0</option>
                                     <option value="1">1</option>
                                     <option value="2">2</option>
                                     <option value="3">3</option>
