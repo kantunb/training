7c24e2bf3a46de67795cc8a052bebd810f01f6b1
Quentin Brouillet <quent07@hotmail.com>
2020-05-03 17:14:44 +0200

    new feature upload image

--- post/create.php
+++ post/create.php
@@ -1,10 +1,33 @@
 <?php
 
+include '../partial/createDb.php';
+include '../partial/navbar.php';
+
 var_dump($_POST);
 
-include '../partial/createDb.php';
+var_dump($_FILES);
 
-$request =  "INSERT INTO whisky (name, category, distillery, bottled, stated_age, strength, flavor, description, rate, image)
+
+$nomOrigine = $_FILES['image']['name'];
+$filename = pathinfo($nomOrigine)['filename'];
+$extension = pathinfo($nomOrigine)['extension'];
+$upload_dir = '../img';
+$newName =  '/' . $_POST['name'] . '-' . uniqid() . '.' . $extension;
+$authorisedExtension = ['jpg', 'jpge', 'png'];
+
+
+if (!$_FILES['image']['error']) {
+        if (in_array($extension, $authorisedExtension)) {
+                move_uploaded_file($_FILES['image']['tmp_name'], $upload_dir . $newName);
+        }
+        else {
+                echo 'Format pas autorisé';
+        }
+}
+
+// die;
+
+$request =  "INSERT INTO whiskies (name, category, distillery, bottled, stated_age, strength, flavor, description, rate, image)
             VALUES ( :name, :category, :distillery, :bottled, :stated_age, :strength, :flavor, :description, :rate, :image)";
 $response = $bdd->prepare($request);
 $response->execute([
@@ -14,19 +37,39 @@ $response->execute([
         'bottled'       => $_POST['bottled'],
         'stated_age'    => $_POST['stated_age'],
         'strength'      => $_POST['strength'],
-        'flavor'        => $_POST['flavor'],
+        'flavor'        => implode(';', $_POST['flavor']),
         'description'   => $_POST['description'],
         'rate'          => $_POST['rate'],
-        'image'         => $_POST['image'],
+        'image'         => $newName,
 
 ]);
 
-$request = "SELECT * FROM whisky";
-$response = $bdd->prepare($request);
-$response->execute();
-$whisky = $response->fetchAll(PDO::FETCH_ASSOC);
+// $request = "SELECT * FROM whiskies";
+// $response = $bdd->prepare($request);
+// $response->execute();
+// $whiskies = $response->fetchAll(PDO::FETCH_ASSOC);
 
-// var_dump($whisky);
 
-header('Location: index.php');
+// header('Location: ../index.php');
+?>
+
+<?php
+// $nomOrigine = $_FILES['monfichier']['name'];
+// $elementsChemin = pathinfo($nomOrigine);
+// $extensionFichier = $elementsChemin['extension'];
+
+//         $repertoireDestination = dirname(__FILE__) . "/";
+//         $nomDestination = "fichier_du_" . date("YmdHis") . "." . $extensionFichier;
+
+//         if (move_uploaded_file(
+//                 $_FILES["monfichier"]["tmp_name"],
+//                 $repertoireDestination . $nomDestination
+//         )) {
+//                 echo "Le fichier temporaire " . $_FILES["monfichier"]["tmp_name"] .
+//                         " a été déplacé vers " . $repertoireDestination . $nomDestination;
+//         } else {
+//                 echo "Le fichier n'a pas été uploadé (trop gros ?) ou " .
+//                         "Le déplacement du fichier temporaire a échoué" .
+//                         " vérifiez l'existence du répertoire " . $repertoireDestination;
+//         }
 ?>
