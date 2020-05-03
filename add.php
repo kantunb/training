e858390944a1fae31d606939006aaef714dfed52
Quentin Brouillet <quent07@hotmail.com>
2020-05-03 10:44:20 +0200

    modify form

--- add.php
+++ add.php
@@ -30,15 +30,15 @@
 
                             <legend>Ajouter un whisky</legend>
 
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for="name">Nom</label>
                                 <input type="text" class="form-control" name="name" id="" aria-describedby="name" value="test_nom" placeholder="" required>
                                 <!-- <small id="name" class="form-text text-muted"></small> -->
-                            </p>
+                            </div>
 
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for="category">Categorie</label>
-                                <select name="category" id="" class="ml-3 aria-describedby="category" required>
+                                <select name="category" id="" class="ml-3 aria-describedby=" category" required>
                                     <option value="">select</option>
                                     <option value="blend" selected>Blended Malt</option>
                                     <option value="single_malt">Single Malt</option>
@@ -46,44 +46,61 @@
                                     <option value="rye">Rye</option>
                                     <option value="tennessee">Tennessee</option>
                                 </select>
-                            </p>
+                            </div>
 
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for=" distillery">Distillerie</label>
                                 <input type="text" class="form-control" name="distillery" id="" aria-describedby="Distillerie" value="test_distillerie" required>
-                            </p>
+                            </div>
 
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for="bottled">Date de mise en bouteille</label>
                                 <input type="number" class="form-control" name="bottled" id="" min="1800" max="2100" aria-describedby="Date de mise en bouteille">
                                 <small id="date_bouteille" class="form-text text-muted">Ex : 2010</small>
-                            </p>
+                            </div>
 
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for="stated_age">Âge</label>
                                 <input type="number" class="form-control" name="stated_age" id="" min="0" max="200" aria-describedby="Âge">
                                 <small id="age_bouteille" class="form-text text-muted">Ex : 15</small>
-                            </p>
+                            </div>
 
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for="strength">Degré d'alcool</label>
                                 <input type="number" class="form-control" name="strength" id="" min="30" max="80" step="0.01" aria-describedby="Degré d'alcool" value="20" required>
                                 <small id="degree_bouteille" class="form-text text-muted">Ex : 42,5</small>
-                            </p>
+                            </div>
 
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for="flavor">Arômes</label>
-                                <input type="text" class="form-control" name="flavor" id="" aria-describedby="Arômes" value="test_aromes" required>
-                            </p>
+                                <ul class="form-check">
+                                    <li>
+                                        <input type="checkbox" class="form-check-input" name="flavor" id="yellow_fruit" value="yellow_fruit" aria-describedby="Arômes" value="test_aromes">
+                                        <label class="form-check-label" for="yellow_fruit">Fruit jaune</label>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox" class="form-check-input" name="flavor" id="smoke" value="smoke" aria-describedby="Arômes" value="test_aromes">
+                                        <label class="form-check-label" for="smoke">Fumé</label>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox" class="form-check-input" name="flavor" id="peat" value="peat" aria-describedby="Arômes" value="test_aromes">
+                                        <label class="form-check-label" for="peat">Tourbe</label>
+                                    </li>
+                                    <li>
+                                        <input type="checkbox" class="form-check-input" name="flavor" id="cereal" value="cereal" aria-describedby="Arômes" value="test_aromes">
+                                        <label class="form-check-label" for="cereal">Céréales</label>
+                                    </li>
+                                </ul>
+                            </div>
 
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for="description">Description</label>
-                                <textarea class="form-control" name="description" id="" aria-describedby="Description"></textarea>
-                            </p>
+                                <textarea class="form-control" name="description" id="" minlength="6" aria-describedby="Description"></textarea>
+                            </div>
 
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for="rate">Note</label>
-                                <select name="rate" id="" class="ml-3 aria-describedby="Note" required>
+                                <select name="rate" id="" class="ml-3 aria-describedby=" Note" required>
                                     <option value="">select</option>
                                     <option value="0" selected>0</option>
                                     <option value="1">1</option>
@@ -97,20 +114,15 @@
                                     <option value="9">9</option>
                                     <option value="10">10</option>
                                 </select>
-                            </p>
+                            </div>
 
-                            <p class="mt-3">
-                                <label for=" owned">J'ai bu ce whisky :</label>
-                                <input type="checkbox" class="form-control" name="distillery" id="" aria-describedby="Distillerie" value="test_distillerie" required>
-                            </p>
-
-                            <p class="mt-3">
+                            <div class="form-group">
                                 <label for="image">Photo</label>
-                                <input type="file" name="image" id="" class="ml-3 aria-describedby="Photo">
-                            </p>
-                            <p class="mt-3">
+                                <input type="file" name="image" id="" class="ml-3 aria-describedby=" Photo">
+                            </div>
+                            <div class="form-group">
                                 <input class="btn btn-primary mt-4" type="submit" value="Ajouter un whisky">
-                            </p>
+                            </div>
                         </fieldset>
                     </form>
 
