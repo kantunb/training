fb991c90309fe19c267247debf98dbdc509dbcf2
Quentin Brouillet <quent07@hotmail.com>
2020-07-02 15:31:57 +0200

    clean insult censorship code

--- php/insult_censorship.php
+++ php/insult_censorship.php
@@ -2,17 +2,6 @@
 $insultes = ['con', 'merde'];
 $sentence = (string)strtolower(readline("Saisissez une phrase: "));
 
-// if(!empty($sentence)) {
-//     foreach ($insultes as $insulte) {
-//         $searchInsulte = strpbrk($insulte, $sentence);
-//         if($searchInsulte) {
-//             $sentence = ucfirst(str_replace($insulte, '*', $sentence) . "\n");
-//         }
-//     }   
-// } else {
-//     exit('Vous n\'avez rien saisi' . "\n");
-// }
-
 if(!empty($sentence)) {
     foreach ($insultes as $insulte) {
         $searchInsulte = strstr($sentence, $insulte);
