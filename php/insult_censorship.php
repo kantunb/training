70a76eaac06a980abf53a9a13740c23317f306c8
Quentin Brouillet <quent07@hotmail.com>
2020-07-03 14:44:58 +0200

    add function convertInsulte

--- php/insult_censorship.php
+++ php/insult_censorship.php
@@ -1,12 +1,24 @@
 <?php
-$insultes = ['con', 'merde'];
+$insultes = ['con', 'merde', 'connard', 'salope', 'salop', 'pute'];
 $sentence = (string)strtolower(readline("Saisissez une phrase: "));
+
+/* Possibilité d'utiliser str_repeat, mais développement d'une fonction pour l'entrainement */
+
+function convertInsulte($insulte) {
+    $lengthInsulte = strlen($insulte);
+    $convertResult = null;
+    for ($i=0; $i < $lengthInsulte ; $i++) {
+        $convertResult = $convertResult . "*";
+    }
+    return $convertResult;
+}
 
 if(!empty($sentence)) {
     foreach ($insultes as $insulte) {
         $searchInsulte = strstr($sentence, $insulte);
+        convertInsulte($insulte);
         if($searchInsulte) {
-            $sentence = str_replace($insulte, '*', $sentence) . "\n";
+            $sentence = ucfirst(str_replace($insulte, convertInsulte($insulte), $sentence)) . "\n";
         }
     }   
 } else {
