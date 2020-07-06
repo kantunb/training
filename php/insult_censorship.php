51429c023ec194350a65597ee442e3e40302ded6
Quentin Brouillet <quent07@hotmail.com>
2020-07-06 10:19:29 +0200

    add regex and error message + replace function insultCensorship + phpdoc

--- php/insult_censorship.php
+++ php/insult_censorship.php
@@ -1,29 +1,69 @@
 <?php
-$insultes = ['con', 'merde', 'connard', 'salope', 'salop', 'pute'];
-$sentence = (string)strtolower(readline("Saisissez une phrase: "));
 
-/* Possibilité d'utiliser str_repeat, mais développement d'une fonction pour l'entrainement */
-
-function convertInsulte($insulte) {
+/**
+ * Description : Replace the letters of the insulte found by stars
+ * 
+ * @param string
+ * 
+ * @return string 
+ */
+function convertInsulte(string $insulte) : string
+{
     $lengthInsulte = strlen($insulte);
-    $convertResult = null;
-    for ($i=0; $i < $lengthInsulte ; $i++) {
-        $convertResult = $convertResult . "*";
-    }
+    $firstLetter = substr($insulte, 0, 1);
+    $convertResult = $firstLetter . str_repeat("*", ($lengthInsulte - 1));
     return $convertResult;
 }
 
-if(!empty($sentence)) {
-    foreach ($insultes as $insulte) {
-        $searchInsulte = strstr($sentence, $insulte);
-        convertInsulte($insulte);
-        if($searchInsulte) {
-            $sentence = ucfirst(str_replace($insulte, convertInsulte($insulte), $sentence)) . "\n";
-        }
-    }   
-} else {
-    exit('Vous n\'avez rien saisi' . "\n");
+/**
+ * @return void
+ */
+function continueOrExit() : void
+{
+    $repeatFunction = (string) readline("Voulez-vous saisir une autre phrase (o / n)?");
+
+    if ($repeatFunction === "o") {
+        insultCensorship();
+    } else if ($repeatFunction === "n") {
+        exit;
+    } else {
+        echo ("La réponse est incorrecte, veuillez recommencer." . "\n");
+        continueOrExit();
+    }
 }
 
+/**
+ * Description : Parse the $insultes array to compare with the $sentence and convert it in $censoredSentence
+ * 
+ * @return void
+ */
+function insultCensorship() : void
+{
+    $insultes = ['con', 'merde', 'connard', 'salope', 'salop', 'batard'];
+    $sentence = (string) strtolower(readline("Saisissez une phrase: "));
+    if (!empty($sentence)) {
+        $censoredSentence = $sentence;
+        $noInsulte = 0;
+        foreach ($insultes as $insulte) {
+            // $searchInsulte = strstr($censoredSentence, $insulte);
+            $searchInsulte = preg_match("~\b$insulte\b~", $censoredSentence);
+            if ($searchInsulte) {
+                convertInsulte($insulte);
+                $censoredSentence = str_replace($insulte, convertInsulte($insulte), $censoredSentence) . "\n";
+            } else {
+                $noInsulte += 1;
+            }
+        }
+        if($noInsulte === count($insultes)){
+            echo ("Votre phrase ne comporte pas d'insultes répertoriées" . "\n");
+        } else {
+            echo ($censoredSentence);
+        }
+    } else {
+        echo ("Vous n'avez rien saisi" . "\n");
+        insultCensorship();
+    }
+    continueOrExit();
+}
 
-echo $sentence;
+insultCensorship();
